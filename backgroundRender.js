cf3192849daf041943b94b00bc783f14178ab0bf
EdLynch <edlynch@email.com>
2019-08-20 22:35:24 +0100

    Mountains Baby!!!

--- backgroundRender.js
+++ backgroundRender.js
@@ -1,53 +1,4 @@
-let mountainSeed = 1;
 
-const mountain = [
-  [0, 400],
-  [10, 400],
-  [50, 370],
-  [0, 350],
-  [0, 320],
-  [60, 350],
-  [60, 250],
-  [0, 270],
-  [20, 220],
-  [40, 200],
-  [40, 160],
-  [40, 160],
-  [40, 200],
-  [42, 150],
-  [90, 120],
-  [60, 240],
-  [60, 340],
-  [90, 220],
-  [90, 360],
-  [15, 400],
-  [130, 360],
-  [90, 360],
-  [92, 280],
-  [140, 240],
-  [140, 190],
-  [95, 110],
-  [200, 200],
-  [140, 190],
-  [135, 300],
-  [130, 350],
-  [130, 360],
-  [215, 330],
-  [215, 330],
-  [200, 300],
-  [215, 330],
-  [200, 300],
-  [240, 300],
-  [300, 320],
-  [400, 300],
-  [340, 300],
-  [240, 300],
-  [250, 250],
-  [160, 250],
-  [150, 340],
-  [250, 250],
-  [200, 200]
-];
 
 //https://medium.com/@ziyoshams/deep-copying-javascript-arrays-4d5fc45a6e3e
 const deepCopy = (arr) => {
@@ -64,17 +15,6 @@ const deepCopy = (arr) => {
     }
   })
   return copy;
-}
-
-let activeMountain = [...mountain];
-
-function getRandomMountain(x, y, flipped = false, scale = 1) {
-  const newMountain = mountain.map(m => [
-    m[0] * scale,
-    (m[1] + getRandomInt(1, 30)) * scale
-  ]);
-  if (flipped) return newMountain.map(o => [o[0] * -1 + 1275, o[1]]);
-  return newMountain;
 }
 
 function renderLine(start, end, colour) {
@@ -120,7 +60,7 @@ function renderSky() {
   ctxs.background.fillRect(0, 0, 1500, 300);
 }
 
-function triangle(p1, p2, p3, fill="#000040") {
+function triangle(p1, p2, p3, fill="#000040", stroke="#00f7ff") {
   // the triangle
   ctxs.middle.beginPath();
   ctxs.middle.moveTo(...p1);
@@ -129,12 +69,12 @@ function triangle(p1, p2, p3, fill="#000040") {
   ctxs.middle.closePath();
 
   // the outline
-  ctxs.middle.lineWidth = 3;
-  ctxs.middle.strokeStyle = "#00f7ff";
+  ctxs.middle.lineWidth = 2;
+  ctxs.middle.strokeStyle = stroke;
  ctxs.middle.stroke();
 
   // the fill color
-  //ctxs.middle.fillStyle = fill;
+  ctxs.middle.fillStyle = fill;
   ctxs.middle.fill();
 }
 
@@ -178,68 +118,6 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-function car(x, y, sX = 1, sY = 1) {
-  function wheels() {
-    ctxs.background.fillStyle = "#b0b0b0";
-
-    ctxs.background.fillRect(x + 15, y + 100, 20, 40);
-    ctxs.background.fillRect(x + 105, y + 100, 20, 40);
-  }
-  function body() {
-    ctxs.background.fillStyle = "#b0b0b0";
-    cube(
-      [x, y + 100],
-      [x + 140, y + 100],
-      [x + 120, y + 120],
-      [x + 20, y + 120],
-      "rgb(255, 255, 255, 0)",
-      "#2e2e2e"
-    );
-    cube(
-      [x, y + 100],
-      [x + 140, y + 100],
-      [x + 120, y + 80],
-      [x + 20, y + 80],
-      "rgb(255, 255, 255, 0)",
-      "#b0b0b0"
-    );
-    cube(
-      [x + 25, y + 80],
-      [x + 115, y + 80],
-      [x + 100, y + 60],
-      [x + 40, y + 60],
-      "#b0b0b0",
-      "rgb(255, 255, 255, 0)"
-    );
-    cube(
-      [x + 35, y + 75],
-      [x + 105, y + 75],
-      [x + 95, y + 65],
-      [x + 45, y + 65],
-      "#2b2b2b",
-      "rgb(255, 255, 255, 0)"
-    );
-  }
-  wheels();
-  body();
-}
-
-function chainedCube(p, scale = 1, x=0,y=0) {
-  
-  p = p.map(pp => [pp[0], pp[1]]);
-
-  cube(p[0], p[1], p[2], p[3], "#00fff7", "#1a013b");
-
-  for (let i = 4; i < p.length; i += 2) {
-    cube(p[i - 2], p[i - 1], p[i], p[i + 1], "#00fff7", "#1a013b");
-  }
-
-}
-
-setInterval(() => {
-  renderBackground(), time++;
-}, 30);
-
 //###############################
 //#############STOLEN############
 //###############################
@@ -250,155 +128,7 @@ const median = arr => {
 };
 
 
-let firstStrand = {coords:[[[250,270],[350,300],[250,320],[150,300]],
-[[150,250],[200,220],[250,270],[150,300]],
-[[150,200],[170,150],[200,220],[150,250]],
-[[100,100],[100,100],[170,150],[150,200]]],scale:1}
 
-
-let strands = [firstStrand]
-
-function renderBackground() {
-  renderFloor();
-  renderGrid(time);
-  renderSun();
-  renderSky();
- // renderMountains();
-
-  mountainList.forEach(m=>drawMountainStrand(m, "#00f7ff"))
-  shouldAddMountain === 0 ? addMountain() : shouldAddMountain--
-  mountainList = transformMountains(mountainList)
-  ctxs.background.save()
-  ctxs.background.scale(.4,.4);
-  ctxs.background.translate(300,380);
-  strands.forEach(strand=>strand.coords.forEach((square=>
-    drawSquare(square)
-  )))
-  //ctxs.background.scale(2,2);
-  ctxs.background.restore()
-  transformStrands()
-  addStrand()
-
-  triangleBlock(0,300,40,10,10)
-  
-  ctxs.middle.save();
-  ctxs.middle.globalCompositeOperation = 'destination-out';
-  triangle([0,450],[400,300],[1000,600],"rgba(0,0,0,0)")
-  ctxs.middle.restore();
-}
-
-let shouldAddMountain = 9;
-
-
-function addStrand(){
-  const lastStrand = deepCopy(strands[strands.length-1].coords)
-  let newStrand = deepCopy(lastStrand)
-
-  newStrand = newStrand.map(square=>square.map(point=>[point[0]+50,point[1]-20]))
-
-  //top point
- // newStrand[3][0] = lastStrand[3][0]
-  //newStrand[3][1] = lastStrand[3][1]
-  newStrand[3][2] = lastStrand[3][2]
-  
-  newStrand[3][2] = newStrand[2][1]
-  newStrand[3][3] = newStrand[2][0]
-
-  newStrand[2][0] = lastStrand[2][1]
-  newStrand[2][3] = lastStrand[2][2]
-
-
-  //second point down
-  newStrand[1][3] = lastStrand[1][2]
-  newStrand[1][0] = lastStrand[1][1]
-  newStrand[1][2] = newStrand[0][1]
-
-  
-  
-  //bottom point
- newStrand[0][3] = lastStrand[0][1]
-// newStrand[0][0] = lastStrand[0][0]
- newStrand[0][2] = lastStrand[0][1]
-
- 
-
- //
-  newStrand[3] = newStrand[3].map(strand => [strand[0],strand[1]+5])
-  newStrand[2] = newStrand[2].map(strand => [strand[0],strand[1]+5])
-  newStrand[1] = newStrand[1].map(strand => [strand[0],strand[1]+5])
-
- strands.push({ coords: newStrand, scale: strands[strands.length-1].scale -= 0.1 })
-}
-
-function transformStrands(){
-  const newStrands = []
-  strands.forEach(strand => {
-    const coords = strand.coords.map(strand => strand.map(cube =>[cube[0]-10,cube[1]+2]))
-    const scale = strand.scale
-
-    
-    coords[3] = coords[3].map(strand => [strand[0],strand[1]-.8])
-    coords[2] = coords[2].map(strand => [strand[0],strand[1]-.8])
-    coords[1] = coords[1].map(strand => [strand[0],strand[1]+.8])
-    coords[0] = coords[0].map(strand => [strand[0],strand[1]+.8])
-
-    newStrands.push({coords, scale})
-  })
-  strands = newStrands
-}
-
-function addMountain(){
-  mountainList.push(createMountainNode(mountainList[mountainList.length-1]))
-  shouldAddMountain = 9
-}
-
-const mountain1 = {x:400,y:300,scale:0}
-const mountains = []
-
-function drawMountain({x,y,scale}){
-  ctxs.background.fillStyle = "#b0b0b0";
-
-  ctxs.background.fillRect(x, y, 100*scale, 50*scale);
-  
-}
-
-function updateMountain(mountain){
-  mountain.x-=5
-  mountain.y-=.3
-  mountain.scale += 0.02
-}
-
-
-let mountainList = [{pos:[895,300],height:5,width:40,point:(getRandomInt(5,30))}]
-
-function createMountainNode(lastNode){
-  return {pos:[lastNode.pos[0]-40,lastNode.pos[1]],height:lastNode.height-6,width:40,point:(getRandomInt(5,30))}
-}
-
-function transformMountains(mountainArray){
-  return mountainArray.map(m=>({...m,pos:[m.pos[0]+4,m.pos[1]],height:m.height+.6}))
-}
-
-function drawMountainStrand(mountain, stroke = "#ff36f2", fill = "#000040") {
-  ctxs.background.beginPath();
-  ctxs.background.moveTo(...mountain.pos);
-  ctxs.background.lineTo(mountain.pos[0]+mountain.width/2,mountain.pos[1]-mountain.point);
-  ctxs.background.lineTo(mountain.pos[0]+mountain.width,mountain.pos[1]+5);
-  ctxs.background.lineTo(mountain.pos[0]+mountain.width,mountain.pos[1]+mountain.height);
-  ctxs.background.lineTo(mountain.pos[0],mountain.pos[1]+mountain.height-5);
-  ctxs.background.closePath();
-  ctxs.background.lineWidth = 5;
-  ctxs.background.strokeStyle = stroke;
-  ctxs.background.stroke();
-
-  ctxs.background.fillStyle = fill;
-  ctxs.background.fill();
-}
-
-function drawTestMountain(x=300,y=300){
-  drawTriangle([[x,y],[x+20,y+20],[x-20,y+20]])
-  drawTriangle([[x,y],[x-20,y+20],[x-10,y+20]])
-}
 
 function drawSquare(points, stroke = "#00f7ff", fill = "#000040") {
   ctxs.background.beginPath();
@@ -415,12 +145,95 @@ function drawSquare(points, stroke = "#00f7ff", fill = "#000040") {
   ctxs.background.fill();
 }
 
+function renderMountain({x,y,scale, alt, left}){
+  ctxs.middle.save()
 
-function triangleBlock(x,y,size,width,height){
-  for(let w = 0; w < width*size; w+=size){
-    for(let h = 0; h < height*size; h+=size){
-      triangle([x+w,y+h],[x+size+w,y+h],[x+size/2+w,y+size+h])
-      triangle([x-size/2+w,y+h+size],[x+w,y+h],[x+size/2+w,y+size+h])
-    }
+  ctxs.middle.translate(x, y);
+  x=0
+  y=0
+ // ctxs.middle.translate(100/scale,100/scale);
+  ctxs.middle.scale(scale,scale)
+  if(left){
+    ctxs.middle.scale(-1,1)
+    ctxs.middle.translate(220/scale, 0);
+  } 
+
+  const light = "#5c5c5c"
+  const mid = "#404040"
+  const dark = "#171717"
+ // ctxs.middle.scale(0.5,0.5)
+
+  const midPointRight = [x+50+alt[0],y+50+alt[1]]
+  const midPointLeft = [x-50+alt[0],y+50+alt[1]]
+  const topPoint = [x+alt[0],y+alt[1]]
+  const pointBottom = [x+alt[0],y-50+alt[1]]
+
+  //Base thin side
+  triangle([x+100,y+100],midPointRight,[x+25,y], dark)
+  triangle([x-100,y+100],midPointLeft,[x-25,y], light)
+
+  //Point
+  triangle(topPoint,midPointRight,pointBottom, dark)
+  triangle(topPoint,midPointLeft,pointBottom, light)
+
+  //Mid
+  triangle([x,y+100],midPointRight,topPoint, dark)
+  triangle([x,y+100],midPointLeft,topPoint, mid)
+
+  //Base Middle
+  triangle([x,y+100],midPointRight,[x+50,y+110], mid)
+  triangle([x,y+100],midPointLeft,[x-50,y+110], light)
+
+  //Base side
+  triangle([x+100,y+100],midPointRight,[x+50,y+110], dark)
+  triangle([x-100,y+100],midPointLeft,[x-50,y+110], light)
+  ctxs.middle.restore()
+}
+
+let mountainsLeft = []
+const mountainsRight = []
+
+function updateMountain(mountain,index){
+  
+  if(mountain.left){
+    mountain.x-=4
+    mountain.y-=.5
+    mountain.scale+=0.01
+    //if(mountain.x<-100) mountainsLeft.splice(index,1)
+  }else{
+    mountain.x+=4
+    mountain.y-=.5
+    mountain.scale+=0.01
+    //if(mountain.x>1500) mountainsRight.splice(index,1)
   }
 }
+
+let mountainCount = 10;
+
+function renderBackground() {
+  ctxs.middle.clearRect(0,0,2000,2000)
+  renderFloor();
+  renderGrid(time);
+  renderSun();
+  renderSky();
+  mountainsLeft.forEach(mountain => renderMountain(mountain))
+  mountainsLeft.forEach((mountain,index) => updateMountain(mountain,index))
+  mountainsRight.forEach(mountain => renderMountain(mountain))
+  mountainsRight.forEach((mountain,index) => updateMountain(mountain,index))
+  if(mountainCount == 0){
+    mountainsLeft.push({x:750,y:300,scale:.05,alt:[getRandomInt(-50,50),getRandomInt(-50,50)],left:true} );
+    mountainsLeft.push({x:750,y:300,scale:.05,alt:[getRandomInt(-50,50),getRandomInt(-50,50)],left:true} );
+    mountainsLeft.sort((a,b)=> b.x-a.x)
+
+    mountainsRight.push({x:750,y:300,scale:.05,alt:[getRandomInt(-50,50),getRandomInt(-50,50)],left:false} );
+    mountainsRight.push({x:750,y:300,scale:.05,alt:[getRandomInt(-50,50),getRandomInt(-50,50)],left:false} );
+    mountainsRight.sort((a,b)=> a.x-b.x)
+    
+    mountainCount=10;
+  }
+  mountainCount--
+}
+
+setInterval(() => {
+  renderBackground(), time++;
+}, 30);
