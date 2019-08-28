918ddae7c8efa4f334baa9194d4940830cbe7636
EdLynch <edlynch@email.com>
2019-08-28 20:17:01 +0100

    mountian time

--- src/obstacles.js
+++ src/obstacles.js
@@ -1,12 +1,38 @@
 let obstacles = []
 
-const pointToPath = (p) => [[p[0]-getGrowth(p[1]),p[1]],[p[0]+getGrowth(p[1]),p[1]],[p[0]+getGrowth(p[1]),p[1]+getGrowth(p[1])],[p[0]-getGrowth(p[1]),p[1]+getGrowth(p[1])]]
+const pointToPath1 = (p) => [
+    [p[0]+5,p[1]-10],
+    [p[0]-5,p[1]-10],
+    [p[0]-7,p[1]-8],
+]
+const pointToPath2 = (p) => [
+    [p[0]+5,p[1]-10],
+    [p[0]-1,p[1]+],
+    [p[0]-5,p[1]-10],
+]
+const pointToPath3 = (p) => [
+    [p[0]+5,p[1]-10],
+    [p[0],p[1]],
+    [p[0],p[1]],
+]
 
-function renderObstacles(){
-    obstacles.forEach(obstacle => drawSquare(pointToPath(obstacle)))
+function drawTriangle(obstacle){
+    triangle(...pointToPath1(obstacle))
+    triangle(...pointToPath2(obstacle))
+    triangle(...pointToPath3(obstacle))
 }
 
-const getGrowth = (y) => y/10
+/*[
+    [p[0]+getGrowth(p[1])/4,p[1]+getGrowth(p[1])/2],
+    [p[0]+getGrowth(p[1]),p[1]+getGrowth(p[1])],
+    [p[0]-getGrowth(p[1]),p[1]+getGrowth(p[1])]
+]*/
+
+function renderObstacles(){
+    obstacles.forEach(obstacle => drawTriangle(obstacle))
+}
+
+const getGrowth = (y) => (y-300)/5
 
 function addObstacle(){
     const initX = getRandomInt(1275/2-100,1275/2+100)
@@ -15,7 +41,7 @@ function addObstacle(){
 
 function computeObstacles(){
     if(getRandomInt(0,25) === 0) addObstacle()
-    obstacles = obstacles.map(o=>[o[0]+getChange(o[2]),o[1]+1,o[2]]).filter(o=>o[1]<600)
+    obstacles = obstacles.map(o=>[o[0]+getChange(o[2]),o[1]+2,o[2]]).filter(o=>o[1]<600)
 }
 
 const getChange =(x) => -(1275/2 - x)/20
